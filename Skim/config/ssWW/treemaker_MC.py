faebeac280557b364156e8ad090ef0d0114a17c9
vmariani <vmariani@cern.ch>
2016-07-05 11:06:34 +0200

    test code Muons

--- Skim/config/ssWW/treemaker_MC.py
+++ Skim/config/ssWW/treemaker_MC.py
@@ -11,7 +11,9 @@ process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 # Source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('/store/data/Run2015D/SingleMuon/AOD/16Dec2015-v1/10000/06CC1B3A-FDA7-E511-B02B-00259073E388.root')
+   # fileNames = cms.untracked.vstring('/store/data/Run2015D/SingleMuon/AOD/16Dec2015-v1/10000/06CC1B3A-FDA7-E511-B02B-00259073E388.root')
+     fileNames = cms.untracked.vstring('/store/mc/RunIISpring16DR80/WW_DoubleScattering_13TeV-pythia8/AODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/50000/54683627-2209-E611-89B9-2C768AAF879E.root')
+
 )
 
 # Geometry and Detector Conditions
@@ -28,7 +30,8 @@ process = CommonFSQFramework.Core.customizePAT.customizeGT(process)
 import CommonFSQFramework.Core.MuonViewsConfigs
 import CommonFSQFramework.Core.METViewsConfigs
 import CommonFSQFramework.Core.VerticesViewsConfigs
-
+import CommonFSQFramework.Core.GenLevelViewsConfigs
+import CommonFSQFramework.Core.TriggerResultsViewsConfigs
 
 process.Tree= cms.EDAnalyzer("CFFTreeProducer")
 process.Tree._Parameterizable__setParameters(
@@ -37,5 +40,11 @@ process.Tree._Parameterizable__setParameters(
         CommonFSQFramework.Core.MuonViewsConfigs.get(["MuonView"]))
 process.Tree._Parameterizable__setParameters(
         CommonFSQFramework.Core.METViewsConfigs.get(["METView"]))
+process.Tree._Parameterizable__setParameters(
+        CommonFSQFramework.Core.GenLevelViewsConfigs.get(["GenPartView"]))
+process.Tree._Parameterizable__setParameters(
+       CommonFSQFramework.Core.TriggerResultsViewsConfigs.get(["SingleMuon"])
+)
+
 
 process = CommonFSQFramework.Core.customizePAT.addTreeProducer(process, process.Tree)
